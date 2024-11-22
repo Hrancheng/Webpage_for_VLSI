cab2223e4ea35d9519594c73cdd5900b136963e1
Haoran <chaoran@umich.edu>
2024-11-22 15:27:32 -0500

    move hyperlink to conference

--- src/components/Research.vue
+++ src/components/Research.vue
@@ -18,13 +18,34 @@
             </div>
             <div class="publications-container">
               <ul>
-                <li><a href="https://dl.acm.org/doi/abs/10.1145/3670474.3685971" target="_blank">Human Language to Analog Layout Using GLayout Layout Automation Framework. <strong>MLCAD’24</strong>.</a></li>
-                <li><a href="https://ieeexplore.ieee.org/abstract/document/10511336" target="_blank">Disrupting Conventional Chip Design through the Open Source EDA Ecosystem. <strong>EDTM’24</strong>.</a></li>
-                <li><a href="https://ieeexplore.ieee.org/abstract/document/10153547" target="_blank">Openfasoc: An open platform towards analog and mixed-signal automation and acceleration of chip design. <strong>ISDCS’23</strong>.</a></li>
-                <li><a href="https://ieeexplore.ieee.org/abstract/document/10181884" target="_blank">An open source compatible framework to fully autonomous digital LDO generation. <strong>ISCAS’23</strong>.</a></li>
-                <li><a href="https://ieeexplore.ieee.org/abstract/document/9816083" target="_blank">An open-source and autonomous temperature sensor in SkyWater 130 nm for comprehensive design space exploration. <strong>SSC-L’22</strong>.</a></li>
-                <li><a href="https://dl.acm.org/doi/abs/10.1145/3400302.3415736" target="_blank">The missing pieces of open design enablement: A recent history of Google efforts. <strong>ICCAD’20</strong>.</a></li>
-                <li><a href="https://dl.acm.org/doi/abs/10.1145/3400302.3415734" target="_blank">Bridging academic open-source EDA to real-world usability. <strong>ICCAD’20</strong>.</a></li>
+                <li>
+                Human Language to Analog Layout Using GLayout Layout Automation Framework. 
+                <a href="https://dl.acm.org/doi/abs/10.1145/3670474.3685971" target="_blank"><strong>MLCAD’24</strong></a>.
+              </li>
+              <li>
+                Disrupting Conventional Chip Design through the Open Source EDA Ecosystem. 
+                <a href="https://ieeexplore.ieee.org/abstract/document/10511336" target="_blank"><strong>EDTM’24</strong></a>.
+              </li>
+              <li>
+                Openfasoc: An open platform towards analog and mixed-signal automation and acceleration of chip design. 
+                <a href="https://ieeexplore.ieee.org/abstract/document/10153547" target="_blank"><strong>ISDCS’23</strong></a>.
+              </li>
+              <li>
+                An open source compatible framework to fully autonomous digital LDO generation. 
+                <a href="https://ieeexplore.ieee.org/abstract/document/10181884" target="_blank"><strong>ISCAS’23</strong></a>.
+              </li>
+              <li>
+                An open-source and autonomous temperature sensor in SkyWater 130 nm for comprehensive design space exploration. 
+                <a href="https://ieeexplore.ieee.org/abstract/document/9816083" target="_blank"><strong>SSC-L’22</strong></a>.
+              </li>
+              <li>
+                The missing pieces of open design enablement: A recent history of Google efforts. 
+                <a href="https://dl.acm.org/doi/abs/10.1145/3400302.3415736" target="_blank"><strong>ICCAD’20</strong></a>.
+              </li>
+              <li>
+                Bridging academic open-source EDA to real-world usability. 
+                <a href="https://dl.acm.org/doi/abs/10.1145/3400302.3415734" target="_blank"><strong>ICCAD’20</strong></a>.
+              </li>
               </ul>
             </div>
           </div>
@@ -42,11 +63,25 @@
             </div>
             <div class="publications-container">
               <ul>
-                <li><a href="https://ieeexplore.ieee.org/abstract/document/10584405" target="_blank">Unlocking Circuits for Quantum With Open Source Silicon at 4K. <strong>SSC-M’24</strong>.</a></li>
-                <li>Zhang, Q. (2024). Domain-Specific Acceleration: From Efficient Vision Processing Hardware to High-Performance Quantum Computing Software (Doctoral dissertation).</li>
-                <li><a href="https://ieeexplore.ieee.org/abstract/document/10379433" target="_blank">A fully integrated, automatically generated DC-DC converter from 398 K down to 23 K in 12 nm FinFET. <strong>SSC-L’24</strong>.</a></li>
-                <li><a href="https://doi.org/10.18434/mds2-2997" target="_blank">CMOS Sky130 Primitives measured at cryogenic temperatures. <strong>NIST’23</strong>.</a></li>
-                <li><a href="https://ieeexplore.ieee.org/abstract/document/10310944" target="_blank">Cryogenic Modeling for Open-Source Process Design Kit Technology. <strong>BCICTS’23</strong>.</a></li>
+                <li>
+                  Unlocking Circuits for Quantum With Open Source Silicon at 4K. 
+                  <a href="https://ieeexplore.ieee.org/abstract/document/10584405" target="_blank"><strong>SSC-M’24</strong></a>.
+                </li>
+                <li>
+                  Zhang, Q. (2024). Domain-Specific Acceleration: From Efficient Vision Processing Hardware to High-Performance Quantum Computing Software (Doctoral dissertation).
+                </li>
+                <li>
+                  A fully integrated, automatically generated DC-DC converter from 398 K down to 23 K in 12 nm FinFET. 
+                  <a href="https://ieeexplore.ieee.org/abstract/document/10379433" target="_blank"><strong>SSC-L’24</strong></a>.
+                </li>
+                <li>
+                  CMOS Sky130 Primitives measured at cryogenic temperatures. 
+                  <a href="https://doi.org/10.18434/mds2-2997" target="_blank"><strong>NIST’23</strong></a>.
+                </li>
+                <li>
+                  Cryogenic Modeling for Open-Source Process Design Kit Technology. 
+                  <a href="https://ieeexplore.ieee.org/abstract/document/10310944" target="_blank"><strong>BCICTS’23</strong></a>.
+                </li>
               </ul>
             </div>
           </div>
@@ -63,8 +98,14 @@
             </div>
             <div class="publications-container">
               <ul>
-                <li><a href="https://ieeexplore.ieee.org/abstract/document/10584369" target="_blank">Open se cura: First silicon results of an auditable and transparent hardware root-of-trust system in 16 nm. <strong>SSC-M’24</strong>.</a></li>
-                <li><a href="https://arxiv.org/pdf/2402.10930" target="_blank">ConSmax: Hardware-Friendly Alternative Softmax with Learnable Parameters. <strong>arXiv’24</strong>.</a></li>
+                <li>
+                  Open se cura: First silicon results of an auditable and transparent hardware root-of-trust system in 16 nm. 
+                  <a href="https://ieeexplore.ieee.org/abstract/document/10584369" target="_blank"><strong>SSC-M’24</strong></a>.
+                </li>
+                <li>
+                  ConSmax: Hardware-Friendly Alternative Softmax with Learnable Parameters. 
+                  <a href="https://arxiv.org/pdf/2402.10930" target="_blank"><strong>arXiv’24</strong></a>.
+                </li>
               </ul>
             </div>
           </div>
@@ -82,8 +123,14 @@
             </div>
             <div class="publications-container">
               <ul>
-                <li><a href="https://ieeexplore.ieee.org/abstract/document/10584418" target="_blank">Rapid Prototyping of Laser-Induced Graphene Sensors With Open-Source Silicon. <strong>SSC-M’24</strong>.</a></li>
-                <li><a href="https://ieeexplore.ieee.org/abstract/document/10639937" target="_blank">LIG-OSS: Integrated Laser-Induced-Graphene Sensor and Open-Source Silicon Chip. <strong>NEMS’24</strong>.</a></li>
+                <li>
+                  Rapid Prototyping of Laser-Induced Graphene Sensors With Open-Source Silicon. 
+                  <a href="https://ieeexplore.ieee.org/abstract/document/10584418" target="_blank"><strong>SSC-M’24</strong></a>.
+                </li>
+                <li>
+                  LIG-OSS: Integrated Laser-Induced-Graphene Sensor and Open-Source Silicon Chip. 
+                  <a href="https://ieeexplore.ieee.org/abstract/document/10639937" target="_blank"><strong>NEMS’24</strong></a>.
+                </li>
               </ul>
             </div>
           </div>
@@ -96,10 +143,22 @@
         <div class="section-content">
           <div class="publications-container">
             <ul>
-              <li><a href="https://ieeexplore.ieee.org/abstract/document/10752803" target="_blank">SSCS PICO Chronicles: Code-a-Chip Travel Grant Awards at VLSI 2024. <strong>SSC-M’24</strong>.</a></li>
-              <li><a href="https://ieeexplore.ieee.org/abstract/document/10584346" target="_blank">SSCS PICO Chronicles: News From the Open Source Community. <strong>SSC-M’24</strong>.</a></li>
-              <li><a href="https://ieeexplore.ieee.org/abstract/document/10024752" target="_blank">SSCS PICO Chronicles: A μProcessor With a 5-GHz Radio. <strong>SSC-M’23</strong>.</a></li>
-              <li><a href="https://ieeexplore.ieee.org/abstract/document/9805638" target="_blank">To Support Open Source Ecosystem Ramp-Up. <strong>SSC-M’22</strong>.</a></li>
+              <li>
+                SSCS PICO Chronicles: Code-a-Chip Travel Grant Awards at VLSI 2024. 
+                <a href="https://ieeexplore.ieee.org/abstract/document/10752803" target="_blank"><strong>SSC-M’24</strong></a>.
+              </li>
+              <li>
+                SSCS PICO Chronicles: News From the Open Source Community. 
+                <a href="https://ieeexplore.ieee.org/abstract/document/10584346" target="_blank"><strong>SSC-M’24</strong></a>.
+              </li>
+              <li>
+                SSCS PICO Chronicles: A μProcessor With a 5-GHz Radio. 
+                <a href="https://ieeexplore.ieee.org/abstract/document/10024752" target="_blank"><strong>SSC-M’23</strong></a>.
+              </li>
+              <li>
+                To Support Open Source Ecosystem Ramp-Up. 
+                <a href="https://ieeexplore.ieee.org/abstract/document/9805638" target="_blank"><strong>SSC-M’22</strong></a>.
+              </li>
             </ul>
           </div>
         </div>
